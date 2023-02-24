facaac7a8d2fe8c0eaf45634da6992afd8863876
Puzz1e00 <baibhavacharya10@gmail.com>
2023-02-24 16:29:03 +0545

    Methods and operations

---
+++ tuple_and_dictionary.py
@@ -0,0 +1,14 @@
+t=(1,2,3,1,2,1,1,4)
+count=t.count(1)
+print(count)
+
+index=t.index(2,3)
+print(index)
+s=sum(t)
+print(s)
+print(max(t))
+print(min(t))
+print(sorted(t))
+d1={}
+print(type(d1))
+student = {"name": "abc","age": 24}
